b886e42d0479313192ed572451b36be8901144b4
Jordy713 <jsuarez208@soyudemedellin.edu.co>
2025-09-01 23:44:20 +0000

    financial.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-def imprimir_y_multiplicar_por_dos(dato):
-    print(dato)
-    resultado = dato * 2
-    print(resultado)
+name = "Jordy"
 
-# Ejemplo
-imprimir_y_multiplicar_por_dos(5)
+print(name)
+
+numero = 3
+
+print(numero)
+
+verdad = True
+
+if verdad:
+    print(name)
+else:
+    print(numero)
+
+listas = [1, 2, 3, 4, 5]
+
+diccionario = {"nombre": "Jordy", "edad": 30}
+
+for item in listas:
+    print(item)
+
+[print(item) for item in listas]
+
+def miprimera_funcion(nombre):
+    print(f"Hola, {nombre}")
